b9a9cff1997510347a282fe36069b6112c17fcec
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-04-22 11:29:52 +0200

    pseudo code

---
+++ index.js
@@ -0,0 +1,37 @@
+//call API
+    //for give 10 words
+
+//add function select random words
+
+//add function to calculate tests
+
+// ---
+//add function to delete keylisten keyboard
+
+//add function to check word proposed if exists
+
+//add function to activate enter
+
+//add function to stock data listen keyboard
+
+//add function toDisplay test with wordGuest length * number test
+
+//display letter in div
+
+//add function to listen the keybord
+        //si good number of data  function to stock listen keyboard => function to stop listen keyboard
+// ---
+
+//check length of word proposed
+    //si ok -> check word
+    //no -> send error message and call function calculates tests
+
+// ---
+//add function to create div in container_word
+
+//add function to give color to letter//
+
+//add function to check position of letter to compare word chose
+            //si good position  function to give color to letter -> green
+            // si good letter function to give color to letter -> jaune
+// ---
